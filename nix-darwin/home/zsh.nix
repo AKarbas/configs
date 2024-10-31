5c2a1e8f412a76c0ff6042d7a4b58cfa34f318af
Amin Karbas <6280244+AKarbas@users.noreply.github.com>
2024-10-30 18:23:35 -0700

    Add gt comp + cleaner zsh conf

--- nix-darwin/home/zsh.nix
+++ nix-darwin/home/zsh.nix
@@ -4,23 +4,43 @@
   pkgs,
   ...
 }: {
+  home = {
+    sessionPath = [
+      "/opt/homebrew/bin/"
+    ];
+    sessionVariables = {
+      KUBECONFIG = "${config.home.homeDirectory}/.kube/config";
+    };
+  };
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
   programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
     autocd = true;
+    autosuggestion.enable = true;
+    enable = true;
+    enableCompletion = true;
+    historySubstringSearch.enable = true;
+    syntaxHighlighting.enable = true;
     shellAliases = {
       l = "ls -ahl";
     };
-    profileExtra = ''
-      export PATH="$PATH:/opt/homebrew/bin/"
-      export KUBECONFIG=~/.kube/config
+    initExtra = ''
+      ###-begin-gt-completions-###
+      _gt_yargs_completions()
+      {
+        local reply
+        local si=$IFS
+        IFS=$'
+      ' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "$${words[@]}"))
+        IFS=$si
+        _describe 'values' reply
+      }
+      command -v compdef >/dev/null || (autoload -Uz compinit && compinit)
+      compdef _gt_yargs_completions gt
+      ###-end-gt-completions-###
     '';
     oh-my-zsh = {
       enable = true;
